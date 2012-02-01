e428be2fce0222609de9d3e6f82a54bc0f5a62bb
Joe Ferraro <jf31782@gmail.com>
2012-02-01 10:25:24 -0500

    add edit project capability

--- Support/app/controllers/project_controller.rb
+++ Support/app/controllers/project_controller.rb
@@ -12,17 +12,20 @@ class ProjectController < ApplicationController
   
   attr_accessor :client
   
-  layout "base", :only => [:index_new, :index_edit]
+  layout "base", :only => [:index_new, :index_edit, :index_new_changeset]
 
   def index_new
     kill_server
     my_json = File.read("#{ENV['TM_BUNDLE_SUPPORT']}/resource/metadata_describe.json")
-    render "_project_new", :locals => { :user_action => params[:user_action], :my_json => my_json, :child_metadata_definition => CHILD_META_DICTIONARY }
+    support_folder = ENV['TM_BUNDLE_SUPPORT']
+    sf = support_folder.gsub(/lib\/../, "")
+    render "_project_new", :locals => { :user_action => params[:user_action], :my_json => my_json, :child_metadata_definition => CHILD_META_DICTIONARY, :support_folder => sf}
   end 
    
   def index_edit    
+    kill_server
     if File.not.exist? "#{ENV['TM_PROJECT_DIRECTORY']}/config/.org_metadata"
-      build_index
+      MavensMate.build_index
     else     
       confirmed = TextMate::UI.request_confirmation(
         :title => "MavensMate",
@@ -31,7 +34,7 @@ class ProjectController < ApplicationController
         :button2 => "No")
     end  
     
-    build_index if confirmed
+    MavensMate.build_index if confirmed
     
     project_array = eval(File.read("#{ENV['TM_PROJECT_DIRECTORY']}/config/.org_metadata")) #=> comprehensive list of server metadata
     
@@ -41,16 +44,15 @@ class ProjectController < ApplicationController
     project_package.remove_namespaces!
     project_package.xpath("//types/name").each do |node|
       object_definition = MavensMate::FileFactory.get_meta_type_by_name(node.text) || MavensMate::FileFactory.get_child_meta_type_by_name(node.text)  
-      #puts object_definition.inspect + "<br/><br/>"
+      #=> ApexClass
       is_parent = !object_definition[:parent_xml_name]
       server_object = project_array.detect { |f| f[:key] == node.text }
-      
       next if server_object.nil? && is_parent
             
       if is_parent
-        server_object[:selected] = true
+        server_object[:selected] = "selected"
         server_object[:select_mode] = (node.previous_element.text == "*") ? "all" : "some"
-        select_all(server_object) if server_object[:select_mode] == "all"
+        MavensMate.select_all(server_object) if server_object[:select_mode] == "all"
         next if server_object[:selected] == "all"     
       end
       
@@ -85,7 +87,7 @@ class ProjectController < ApplicationController
         end
         child_object = server_object[:children].detect {|f| f[:key] == prev_node.text }
         child_object[:selected] = "selected" if child_object.not.nil?
-        select_all(child_object) if object_definition[:child_xml_names]
+        MavensMate.select_all(child_object) if object_definition[:child_xml_names]
         prev_node = prev_node.previous_element || nil
       end
       
@@ -113,7 +115,7 @@ class ProjectController < ApplicationController
     
     render "_project_edit", :locals => { :package => project_package, :project_array => project_array, :child_metadata_definition => CHILD_META_DICTIONARY, :pname => pconfig['project_name'], :pun => pconfig['username'], :ppw => password, :pserver => pconfig['environment'] }
   end
-  
+    
   #updates current project
   def update
     begin
@@ -136,8 +138,8 @@ class ProjectController < ApplicationController
       TextMate.call_with_progress( :title => "MavensMate", :message => "Validating Salesforce.com Credentials" ) do
         self.client = MavensMate::Client.new({ :username => params[:un], :password => params[:pw], :endpoint => params[:server_url] })
       end
-      $stdout.flush
-      flush
+      #$stdout.flush
+      #flush
       if ! self.client.sid.nil? && ! self.client.metadata_server_url.nil?
         puts "<input type='hidden' value='#{self.client.sid}' id='sid'/>"
         puts "<input type='hidden' value='#{self.client.metadata_server_url}' id='murl'/>"
@@ -156,22 +158,27 @@ class ProjectController < ApplicationController
       result = MavensMate.new_project(params)
       return if result.nil?
       kill_server unless ! result[:is_success]
+      MavensMate.close_all_html_windows unless ! result[:is_success]
       render "_project_new_result", :locals => { :message => result[:error_message], :success => result[:is_success] }
     rescue Exception => e
       TextMate::UI.alert(:warning, "MavensMate", e.message)
     end
   end
-  
+    
   #checks out project from SVN, associates Salesforce.com server credentials  
   def checkout
-    result = MavensMate.checkout_project(params)
-    render "_project_new_result", :locals => { :message => result[:error_message], :success => result[:is_success] }
+    begin
+      result = MavensMate.checkout_project(params)
+      return if result.nil?
+      kill_server unless ! result[:is_success]
+      MavensMate.close_all_html_windows unless ! result[:is_success]    
+      render "_project_new_result", :locals => { :message => result[:error_message], :success => result[:is_success] } 
+    rescue Exception => e
+      TextMate::UI.alert(:warning, "MavensMate", e.message)
+    end
   end
   
-  #starts TCP server to handle communication between html page and metadata api results
-  def start_server
-    #TODO: rewrite using thin
-    
+  def start_tcp_server
     exit if fork            # Parent exits, child continues.
     Process.setsid          # Become session leader.
     exit if fork            # Zap session leader.
@@ -181,81 +188,89 @@ class ProjectController < ApplicationController
     pid = fork do
       webserver = TCPServer.new('127.0.0.1', 7125)
       while (session = webserver.accept)
-         session.print "HTTP/1.1 200/OK\r\nContent-type:application/json\r\n\r\n"
-         request = session.gets
-         tr = request.gsub(/GET\ \//, '').gsub(/\ HTTP.*/, '')
-         params = tr[tr.index("?")+1,tr.length-1]
-         ps = params.split("&")
-         sid = ""
-         murl = ""
-         meta_type = ""
-         ps.each { |param|
-           pair = param.split("=")
-           if pair[0] == "sid"
-             sid = pair[1]
-           elsif pair[0] == "murl"
-             murl = pair[1]
-           elsif pair[0] == "key"
-             meta_type = pair[1]
+         begin 
+           session.print "HTTP/1.1 200/OK\r\nContent-type:application/json\r\n\r\n"
+           request = session.gets
+           request.gsub!(/GET\ \//, '').gsub!(/\ HTTP.*/, '')
+           request_params = request[request.index("?")+1,request.length-1]
+           response = []
+           if request_params.include? "vc_type" and request_params.include? "vc_url" and request_params.include? "svn_un" and request_params.include? "svn_pw"
+             response = handle_vc_request(request_params)
+             session.puts response.to_json 
+           else
+             response = handle_metadata_request(request_params)
+             session.puts response
            end
-         }
-         cleanmurl = URI.unescape(murl)
-         begin
-           client = MavensMate::Client.new({ :sid => sid, :metadata_server_url => cleanmurl })
-           meta_list = client.list(meta_type, false)
-           session.puts meta_list
          rescue Exception => e
-           #session.print e.message + "\n" + e.backtrace.join("\n")
-           #session.close
+           session.puts e.message + "\n" + e.backtrace.join("\n")
          end
          session.close
       end
     end   
     Process.detach(pid)
-    puts "<input type='hidden' value='#{pid}' id='pid'/>"
+    puts "<input type='hidden' value='#{pid}' id='pid'/>"    
   end
-      
-  private
-    
-    #builds server index and stores in .org_metadata
-    def build_index
-      #mhash = eval(File.read("#{ENV['TM_BUNDLE_SUPPORT']}/resource/metadata_trim.txt"))
-      mhash = eval(File.read("#{ENV['TM_BUNDLE_SUPPORT']}/resource/metadata.txt"))
-      mhash.sort! { |a,b| a[:xml_name].downcase <=> b[:xml_name].downcase } 
-      project_array = []
-      progress = 0
-      threads = []      
-      TextMate.call_with_progress(:title =>'MavensMate',
-               :summary => 'Building server metadata index',
-               :details => 'This could take a while, but it\'ll all be worth it!',
-               :indeterminate => true ) do |dialog|        
-        client = MavensMate::Client.new
-        mhash.each do |metadata|         
-          threads << Thread.new {
-            thread_client = MavensMate::Client.new({ :sid => client.sid, :metadata_server_url => client.metadata_server_url })
-            #progress = progress + 100/mhash.length
-            #dialog.parameters = {'summary' => 'Retrieving '+metadata[:xml_name],'progressValue' => progress }
-            begin   
-              project_array.push({
-                :title => metadata[:xml_name],
-                :key => metadata[:xml_name],
-                :isLazy => false,
-                :isFolder => true,
-                :selected => false,
-                :children => thread_client.list(metadata[:xml_name], false, "array"),
-                :inFolder => metadata[:in_folder],
-                :hasChildTypes => metadata[:child_xml_names] ? true : false
-              })
-            rescue  Exception => e
-              puts e.message + "\n" + e.backtrace.join("\n")  
-            end
-          }        
-        end
-        threads.each { |aThread|  aThread.join }
-      end                                            
-      project_array.sort! { |a,b| a[:title].downcase <=> b[:title].downcase }
-      File.open("#{ENV['TM_PROJECT_DIRECTORY']}/config/.org_metadata", 'w') {|f| f.write(project_array.inspect) }
+  
+  def handle_vc_request(request_params)
+    svn_un, svn_pw, vc_type, vc_url = nil, nil, nil, nil
+    param_array = request_params.split("&")
+    param_array.each do |param|
+      p = param.split("=")
+      if p[0] == "vc_url"
+        vc_url = URI.unescape(p[1])
+        vc_url.gsub!(" ", "/\ ") 
+        vc_url.chomp!
+        vc_url.chop! if vc_url[vc_url.length-1,1] == "/"
+      elsif p[0] == "vc_type"
+        vc_type = p[1].downcase!
+      elsif p[0] == "svn_un"
+        svn_un = p[1]
+        svn_un.chomp! if svn_un.not.nil?
+      elsif p[0] == "svn_pw"
+        svn_pw = p[1]
+        svn_pw.chomp! if svn_pw.not.nil?
+      end  
     end
+    
+    require 'rubygems'
+    require 'nokogiri'
+    opts = []
+    if vc_type == "svn"
+      response = %x{svn list --xml --username #{svn_un} --password #{svn_pw} '#{vc_url}'}                    
+      doc = Nokogiri::XML(response)
+      doc.remove_namespaces!
+      doc.xpath("//entry/name").each do |node|
+        opts.push({:url => vc_url+"/"+node.text, :title => node.text})
+      end 
+    elsif vc_type == "git"
+      response = %x{git ls-remote '#{vc_url}'}         
+      response.split("\n").each do |branch|
+        branch_name = branch.split("\t")[1]
+        opts.push({ :url => branch_name, :title => branch_name })
+      end 
+    end
+    return opts
+  end
+  
+  def handle_metadata_request(request_params)
+    param_array = request_params.split("&")
+    sid, metadata_server_url, meta_type = "", "", ""
+    param_array.each { |param|
+      pair = param.split("=")
+      if pair[0] == "sid"
+        sid = pair[1]
+      elsif pair[0] == "murl"
+        metadata_server_url = pair[1]
+      elsif pair[0] == "key"
+        meta_type = pair[1]
+      end 
+    }
+    metadata_server_url = URI.unescape(metadata_server_url)
+    client = MavensMate::Client.new({ :sid => sid, :metadata_server_url => metadata_server_url })
+    return client.list(meta_type, false)     
+  end
+        
+  private
     
     #stops TCP server proc
     def kill_server
@@ -264,23 +279,4 @@ class ProjectController < ApplicationController
       end
     end
     
-    #selects all metadata in tree
-    def select_all(obj)
-      begin
-        obj[:children].each do |child|
-          child[:selected] = "selected"
-          next if ! child[:children]
-          child[:children].each do |grand_child|
-            grand_child[:selected] = "selected"
-            next if ! grand_child[:children]
-            grand_child[:children].each do |great_grand_child|
-              great_grand_child[:selected] = "selected"
-            end
-          end
-        end
-      rescue
-
-      end
-    end
-
 end
